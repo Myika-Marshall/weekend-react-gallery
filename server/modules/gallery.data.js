c1a23b04e84f256e905e7ac77bc3737871e427f0
Myika Marshall <myika.marshall@gmail.com>
2021-11-30 05:08:20 -0600

    added Axios-get route to app.jsx

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/AuntAnn.jpeg', description: 'My inspiration: photo of a my favourite Aunt, Gwendolyn Diane Jones during her fight with cancer. Circa 2019.', likes: 0 },
-    {id: 2, path: 'images/Baking.png', description: 'Random baking day experiment! Youtube rules!', likes: 0 },
-    {id: 3, path: 'images/FavChemopic.png', description: 'One of my favorite photos- physically dying, spiritually thriving! .', likes: 0 },
-    {id: 4, path: 'images/June2019.png', description: 'Photo me during my battle with cancer, holding my newborn baby!.', likes: 0 }
+    {id: 1, path: 'images/goat_small.jpg', description:'Random photo of a goat', likes: 0 },
+    {id: 2, path: 'images/AuntAnn.jpeg', description: 'My inspiration: photo of a my favourite Aunt, Gwendolyn Diane Jones during her fight with cancer. Circa 2019.', likes: 0 },
+    {id: 3, path: 'images/Baking.png', description: 'Random baking day experiment! Youtube rules!', likes: 0 },
+    {id: 4, path: 'images/FavChemopic.png', description: 'One of my favorite photos- physically dying, spiritually thriving! .', likes: 0 },
+    {id: 5, path: 'images/June2019.png', description: 'Photo me during my battle with cancer, holding my newborn baby!.', likes: 0 }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios'
 import './App.css';
+import GalleryList from '../GalleryList/GalleryList';
 
 function App() {
+  //create an axios to retrieve photos
+  const [pictures, setPictures] = useState([]);
+
+  useEffect (() =>{
+    getPictures();
+  },[],
+  )
+
+  let getPictures = () => {
+    axios.get('/gallery')
+    .then((response) => {
+      console.log('axios response is:', response)
+      setPictures(response.data);
+      console.log(response.data); 
+    }).catch((err) =>{
+      console.log('axios err:' err);
+    })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <div className = "pictures">
+          <GalleryList pictures = {pictures} getPictures = {getPictures/>}
+      </div>
       </div>
     );
 }
