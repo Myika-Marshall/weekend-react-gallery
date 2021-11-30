a88e68f58984f63a9a576e748f5732f6497a951f
Myika Marshall <myika.marshall@gmail.com>
2021-11-30 03:56:05 -0600

    added images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/AuntAnn.jpeg', description: 'My inspiration: photo of a my favourite Aunt, Gwendolyn Diane Jones during her fight with cancer. Circa 2019.', likes: 0 },
+    {id: 2, path: 'images/Baking.png', description: 'Random baking day experiment! Youtube rules!', likes: 0 },
+    {id: 3, path: 'images/FavChemopic.png', description: 'One of my favorite photos- physically dying, spiritually thriving! .', likes: 0 },
+    {id: 4, path: 'images/June2019.png', description: 'Photo me during my battle with cancer, holding my newborn baby!.', likes: 0 }
 ];
 
 module.exports = galleryItems;
